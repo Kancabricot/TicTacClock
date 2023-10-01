bc1442b9acfafb3609d8ade4a393378da5bb2ff8
Kancabricot <timothee.fontan@gmail.com>
2023-10-01 17:42:54 +0200

    Initial commit

---
+++ Source/TicTacClock/TicTacClock.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TicTacClock.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TicTacClock, "TicTacClock" );
+ 
---
+++ Source/TicTacClock/TicTacClockGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TicTacClockGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATicTacClockGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATicTacClockGameMode();
+};
+
+
+
