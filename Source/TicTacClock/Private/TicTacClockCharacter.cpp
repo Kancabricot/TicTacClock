ae27624215a7fb9d77addc4b00f53838697cbf91
NathanPey <jeanbobdelaforet@gmail.com>
2023-10-16 17:59:03 +0200

    oui

--- Source/TicTacClock/Private/TicTacClockCharacter.cpp
+++ Source/TicTacClock/Private/TicTacClockCharacter.cpp
@@ -75,25 +75,7 @@ void ATicTacClockCharacter::SetupPlayerInputComponent(class UInputComponent* Pla
 
 void ATicTacClockCharacter::Move(const FInputActionValue& Value)
 {
-	// input is a Vector2D
-	FVector2D MovementVector = Value.Get<FVector2D>();
 
-	if (Controller != nullptr)
-	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// get forward vector
-		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-	
-		// get right vector 
-		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-
-		// add movement 
-		AddMovementInput(ForwardDirection, MovementVector.Y);
-		AddMovementInput(RightDirection, MovementVector.X);
-	}
 }
 
 
